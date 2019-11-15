ae66f2a473ee3f8a5a9a6c904ae17fec5d105f3d
Adler <n_adler@ats-hr420-dm13.fc.ca>
2019-11-15 13:55:11 -0500

    Made two graphs through HTML and Python

---
+++ data/newchart.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+
+hours =[4, 8]
+activities =["sleep", "work"]
+colors = ["r", "g"]
+
+plt.pie(hours, labels=activities, colors=colors, startangle=90, autopct="%.1f%%")
+plt.show()
